a53fb293e26252807a9f242d0a269c4a9c92ee2f
Nischal <nischalbaskota08@gmail.com>
2024-02-27 01:15:30 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      posts:[]
+    }
+  }
+componentDidMount(){
+  const url="https://jsonplaceholder.typicode.com/posts";
+  fetch(url).then(response=> response.json()).then(json=>this.setState({posts:json}))
+  // console.log(url);
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">Blog posts</h1>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            Featured
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function app {
+//   console.log("S"); ///normal func
+// }
+
+// const demo=()=>{      /// Arrow func
+//   console.log("s");
+// }
